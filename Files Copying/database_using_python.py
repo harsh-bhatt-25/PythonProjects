3d63f29a8245c322ee3f23be75dff6026af3093d
harsh <harsh.bhattcr@gmail.com>
2020-10-01 18:42:39 -0400

    Adding to SQL database

---
+++ Files Copying/database_using_python.py
@@ -0,0 +1,56 @@
+import mysql.connector as MySQL
+import mysql.connector as errorcode
+import os
+from csv import reader, writer
+import random
+import time
+
+conn = MySQL.connect(
+    host="localhost",
+    database="business",
+    user="root",
+    password="password"
+)
+cur = conn.cursor()
+
+DB_NAME = "business"
+# TABLES = {}
+#
+# TABLES["Cities"] = (
+#     "CREATE TABLE `Cities`("
+#     "`PostOfficeName` varchar(45),"
+#     "`Pincode` int(10),"
+#     "`District Name` varchar(45),"
+#     "`City` varchar(45),"
+#     "`State` varchar(45)"
+#     ") ENGINE=InnoDB")
+#
+# for table_name in TABLES:
+#     table_description = TABLES[table_name]
+#     try:
+#         print("Creating Table {}: ".format(table_name), end='')
+#         cur.execute(table_description)
+#     except MySQL.connector.Error as err:
+#         if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+#             print("Already Exists")
+#         else:
+#             print(err.msg)
+#     else:
+#         print("OK")
+
+cityfile = open(r"C:\Users") # a csv file
+csvReader = reader(cityfile)
+next(csvReader)
+
+for row in csvReader:
+    # try:
+    print(row)
+    cur.execute("INSERT INTO color VALUES (%s, %s)",
+                (row[0], row[1]))
+    conn.commit()
+    # except:
+    #     break
+
+cur.close()
+conn.commit()
+conn.close()
